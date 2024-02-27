48f7da15f01877d5620e11701e38091de361512b
Ismael Padilla <padillaismael92@gmail.com>
2024-02-26 23:48:21 -0300

    Handle concurrent clients

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,27 +1,35 @@
-use std::{
-    io::{self, Read, Write},
+use std::io::{self, Read, Write};
+
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
 };
 
-fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:8080")?;
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
     println!("Server started on port 8080");
 
-    for stream in listener.incoming() {
-        // println!("for");
-        handle_client(stream?);
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            handle_client(socket).await;
+        });
     }
-    Ok(())
 }
 
-fn handle_client(mut stream: TcpStream) {
+async fn handle_client(mut stream: TcpStream) {
     println!("Connection established!");
-    stream.write_all(b"Connection established!");
+    stream.write_all(b"Connection established!\n").await;
 
     let mut buf = vec![0; 1024];
-    for result in stream.read(&mut buf) {
-        println!("{:?}", result);
-        print!("Read: ");
+    'outer: loop {
+        let result = stream.read(&mut buf).await.unwrap();
+        if result == 0 {
+            println!("Connection closed");
+            break 'outer;
+        }
+        print!("Read {:?} bytes: ", result);
         io::stdout().write_all(&buf[0..result]).unwrap();
     }
 }
